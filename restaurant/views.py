47109bfcdddec843c9039911a4cc26dbd9330128
Patrik Duda <patrikduda94@gmail.com>
2023-10-26 13:17:56 +0200

    Add IsAuthenticated class to permisson_classes attribute in Menu views

--- restaurant/views.py
+++ restaurant/views.py
@@ -17,6 +17,7 @@ class BookingViewSet(ModelViewSet):
 class MenuItemView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
